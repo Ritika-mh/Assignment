5c504f4949320e883278bdb919a06d6392c9fbe0
Ritika-mh <ritika.jawale@gmail.com>
2023-01-19 12:36:57 +0530

    Even n odd

---
+++ Assignment1/Assignment1_2.py
@@ -0,0 +1,11 @@
+#Demonstration of indetification even or odd no"
+def chkNum():
+	i=int(input("enter no"))
+	if i%2==0:
+		print("no is even")
+	else:
+		print("no is odd")
+	
+
+if __name__ =="__main__":
+	chkNum()
